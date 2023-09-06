0584c5054df52a9194979c9b86857886baafc02d
Fahad <fahad@datasciencedojo.com>
2023-09-06 13:52:52 -0700

    Added bootcamp_class_14 code and renamed a folder to bootcamp_class_01

---
+++ bootcamp_class_14/dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+let myObj = {
+    name: "Hello"
+};
+console.log(myObj);
+let user;
+user = {
+    name: "Hello",
+    age: 56
+};
---
+++ bootcamp_class_14/src/index.ts
@@ -0,0 +1,12 @@
+let myObj = {
+    name: "Hello"
+}
+
+console.log(myObj)
+
+let user: {name: string, age: number};
+
+user = {
+    name: "Hello",
+    age: 56
+}
